1b8aa620b3660dd63d2be6a7f769058fb322ac5c
Kalong <kalongdulkhadir@gmail.com>
2014-03-03 10:58:15 +0100

    Ändrat projekt...

--- kd322b_testprojekt/src/kd322b_testprojekt/projekt.java
+++ kd322b_testprojekt/src/kd322b_testprojekt/projekt.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 public class projekt extends JFrame {
 
@@ -35,8 +36,11 @@ public class projekt extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(100, 100, 115, 29);
+		contentPane.add(btnNewButton);
 	}
-
 }
